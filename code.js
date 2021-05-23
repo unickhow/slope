a2e844bf9fdf052d8d353038f6f067405838bef4
unick_how <unick.zhow@gmail.com>
2021-05-23 18:55:56 +0800

    💡 feat: basic pile up number

--- code.js
+++ code.js
@@ -1,29 +1,44 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
-        }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
-    }
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+figma.showUI(__html__);
+function setBaseNumberPileUp(base = '0', order = 'asc', length) {
+    const baseNumber = +base;
+    let stack = 0;
+    let res;
+    return () => {
+        res = order === 'asc'
+            ? baseNumber + stack
+            : baseNumber - stack;
+        stack += 1;
+        return res.toString().padStart(length, '0');
+    };
+}
+figma.ui.onmessage = (payload) => __awaiter(this, void 0, void 0, function* () {
+    yield figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+    const { prefix, baseNumber, order, type } = payload;
+    const nodes = figma.currentPage.selection;
+    const nodesLength = nodes.length;
+    if (type === 'generate') {
+        const isInvalid = nodes.some(node => node.type !== 'TEXT');
+        if (isInvalid) {
+            return "Select a single text node.";
+        }
+        const genNextNumber = setBaseNumberPileUp(baseNumber, order, baseNumber.length);
+        for (let i = 0; i < nodesLength; i += 1) {
+            const result = `${prefix}${genNextNumber()}`;
+            nodes[i].characters = result;
+            nodes[i].name = result;
+        }
+    }
+    // on cancel
+    if (type === 'cancel') {
+        figma.closePlugin();
+    }
+});
